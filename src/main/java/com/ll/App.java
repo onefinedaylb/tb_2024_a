aee8256b2651e6a415b1bed9bba91698ad73b923
onefinedaylb <psssjp22@example.com>
2024-04-30 02:14:35 +0900

    4단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 class App {
     void run() {
         System.out.println("== 명언 앱 ==");
+        int lastQuotationId = 0;
 
         while(true){
             System.out.println("명령) ");
@@ -18,10 +19,12 @@ class App {
             else if(cmd.equals("등록")){
                 System.out.print("명언 : ");
                 String content = scanner.nextLine();
+
                 System.out.print("작가 : ");
                 String authorName = scanner.nextLine();
+                lastQuotationId++;
 
-                System.out.println("1번 명언이 등록되었습니다.");
+                System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotationId);
             }
         }
 
